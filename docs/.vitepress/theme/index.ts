d8e738735bbc28513ffbf38b866ac6197b27a1fe
NekoTC <xtwg17192522@outlook.com>
2025-10-24 00:25:42 +0800

    Add some styles

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,59 @@
 /* .vitepress/theme/index.ts */
+import type { App } from 'vue';
+import { Router } from 'vitepress'
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
 import DefaultTheme from 'vitepress/theme'
 import './style/index.css'
 
+import mediumZoom from 'medium-zoom';
+import googleAnalytics from 'vitepress-plugin-google-analytics'
+
+let homePageStyle: HTMLStyleElement | undefined
+
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app , router}: { app: App , router: Router}) {
+    googleAnalytics({
+      id: 'G-C74JN0JZBD', //跟踪ID，在analytics.google.com注册即可
+    })
+    if (typeof window !== 'undefined') {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/'),
+        { immediate: true },
+      )
+    }
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+}
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle) return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
+  } else {
+    if (!homePageStyle) return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
+  }
 }
